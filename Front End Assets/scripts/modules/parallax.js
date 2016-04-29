25e50080261b0fe5481c849c719e1f5538a7bcde
Austin Smith <austinsmith2011@gmail.com>
2016-04-29 16:24:17 -0500

    updating parallax.js

--- Front End Assets/scripts/modules/parallax.js
+++ Front End Assets/scripts/modules/parallax.js
@@ -1,12 +1,6 @@
 // add data-scroll-speed as an attribute to the html element you want to affect
 
-
-$(window).on('scroll', function () {
-    var scrollTop     = $(window).scrollTop(),
-        elementOffset = $('#my-element').offset().top,
-        distance      = (elementOffset - scrollTop);
-    });
-    
+//calculates element's distance from top of viewport, not top of page    
 $.fn.moveIt = function(){
   var $window = $(window);
   var instances = [];
@@ -18,7 +12,8 @@ $.fn.moveIt = function(){
   window.onscroll = function(){
     var scrollTop = $window.scrollTop();
     instances.forEach(function(inst){
-      inst.update(scrollTop);
+      var distance = inst.el.offset().top - scrollTop;
+      inst.update(distance);
     });
   }
 }
@@ -28,9 +23,9 @@ var moveItItem = function(el){
   this.speed = parseInt(this.el.attr('data-scroll-speed'));
 };
 
-moveItItem.prototype.update = function(scrollTop){
-  var pos = scrollTop / this.speed;
-  this.el.css('transform', 'translateY(' + -pos + 'px)');
+moveItItem.prototype.update = function(y){
+  var pos = y / this.speed;
+  this.el.css('transform', 'translateY(' + pos + 'px)');
 };
 
 $(function(){
